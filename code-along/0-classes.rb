3dc44ed9a38b12ce73eedecf71d21817c9196075
dhorning0028 <dustin.horning@kellogg.northwestern.edu>
2022-02-03 01:15:00 +0000

    practice

--- code-along/0-classes.rb
+++ code-along/0-classes.rb
@@ -4,7 +4,30 @@
 # rails runner code-along/0-classes.rb
 
 # String Class
+dustins_favorite_food = "sushi"
+puts dustins_favorite_food.upcase
+puts dustins_favorite_food.class
+
+brians_favorite_food = "tacos"
+puts brians_favorite_food
+
+number_of_tacos = 5
+puts number_of_tacos
+puts number_of_tacos.class
 
 # String Methods
 
+
+
 # Custom Class
+class Dog
+    def speak
+        puts "woof"
+    end
+end
+
+Cabot = Dog.new
+Cabot.class
+Cabot.speak
+Charlie = Dog.new
+Charlie.speak
